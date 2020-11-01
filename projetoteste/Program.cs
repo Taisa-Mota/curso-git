821e94c24f9d073d8657a04f518004d7c0f89055
Taisa Mota <taisa.p4.2011@hotmail.com>
2020-11-01 12:13:31 -0300

    Acrescentada uma msg de bom dia

--- projetoteste/Program.cs
+++ projetoteste/Program.cs
@@ -7,6 +7,7 @@ namespace projetoteste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!!!");
+            Console.WriteLine("Bom Dia");
             Console.ReadLine();
         }
     }
